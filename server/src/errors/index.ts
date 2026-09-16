d67bc6079cf1a39462bae25da5464c6098cb6158
Aditya Jha <141293751+ad-itya07@users.noreply.github.com>
2026-09-16 10:40:36 +0530

    feat: add custom error classes for improved error handling

---
+++ server/src/errors/index.ts
@@ -0,0 +1,6 @@
+export { AppError } from './AppError';
+export { DatabaseError } from './DatabaseError';
+export { ValidationError } from './ValidationError';
+export { NotFoundError } from './NotFoundError';
+export { AuthenticationError } from './AuthenticationError';
+export { AuthorizationError } from './AuthorizationError';
